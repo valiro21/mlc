a5ceb7bca3d74b4a389377481fe02288c668d146
Valentin Rosca <rosca.valentin2012@gmail.com>
2017-04-17 15:57:23 +0300

    Prepared SQLAlchemy file

---
+++ DB/__init__.py
@@ -0,0 +1,19 @@
+# Copyright © 2017 Valentin Rosca <rosca.valentin2012@gmail.com>
+# Copyright © 2017 Cosmin Pascaru <cosmin.pascaru2@gmail.com>
+# Copyright © 2017 Andrei Netedu <andrei.netedu2009@gmail.com>
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    firstname = Column(String)
+    lastname = Column(String)
+    password = Column(String)
+
+    def __repr__(self):
+        return "<User(firstname='%s', lastname='%s', password='%s')>" % (
+                             self.firstname, self.lastname, self.password)
