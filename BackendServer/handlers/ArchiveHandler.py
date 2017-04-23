316b852061cfc1b55428bd511e776b8386496fe1
Andrei Netedu <andrei.netedu2009@gmail.com>
2017-04-23 15:36:41 +0300

    Random Updates, the blue things, Database Init, added Copyright
    Signed-off-by: Andrei Netedu <andrei.netedu2009@gmail.com>

---
+++ BackendServer/handlers/ArchiveHandler.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+"""RootHandler for BackendWebServer."""
+
+import tornado.web
+
+# Copyright © 2017 Valentin Rosca <rosca.valentin2012@gmail.com>
+# Copyright © 2017 Cosmin Pascaru <cosmin.pascaru2@gmail.com>
+# Copyright © 2017 Andrei Netedu <andrei.netedu2009@gmail.com>
+
+
+class ArchiveHandler(tornado.web.RequestHandler):
+    """Root Handler for BackendWebServer."""
+
+    def data_received(self, chunk):
+        pass
+
+    def get(self):
+        self.render("archive.html")
