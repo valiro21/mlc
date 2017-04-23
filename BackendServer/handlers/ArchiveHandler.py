8f90c1a8bd67bd71ac1036c56c4fc5d88475335f
Andrei Netedu <andrei.netedu2009@gmail.com>
2017-04-23 15:32:59 +0300

    Random Updates, the blue things
    Signed-off-by: Andrei Netedu <andrei.netedu2009@gmail.com>

--- BackendServer/handlers/ArchiveHandler.py
+++ BackendServer/handlers/ArchiveHandler.py
@@ -3,16 +3,16 @@
 
 import tornado.web
 
-
 # Copyright © 2017 Valentin Rosca <rosca.valentin2012@gmail.com>
 # Copyright © 2017 Cosmin Pascaru <cosmin.pascaru2@gmail.com>
 # Copyright © 2017 Andrei Netedu <andrei.netedu2009@gmail.com>
 
-class MainHandler(tornado.web.RequestHandler):
+
+class ArchiveHandler(tornado.web.RequestHandler):
     """Root Handler for BackendWebServer."""
 
     def data_received(self, chunk):
         pass
 
     def get(self):
-        self.render("main.html")
+        self.render("archive.html")
