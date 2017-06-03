eb34b3e649b5f6dffd97426b91ca4bd2c487e659
Cosmin Pascaru <cosmin.pascaru2@gmail.com>
2017-06-03 13:33:14 +0300

    Add dataset full functionality

--- AdminServer/handlers/DatasetHandler.py
+++ AdminServer/handlers/DatasetHandler.py
@@ -3,7 +3,11 @@
 # Copyright © 2017 Valentin Rosca <rosca.valentin2012@gmail.com>
 # Copyright © 2017 Cosmin Pascaru <cosmin.pascaru2@gmail.com>
 # Copyright © 2017 Andrei Netedu <andrei.netedu2009@gmail.com>
+import os
+import traceback
+import zipfile
 
+from io import StringIO, BytesIO
 from sqlalchemy.exc import SQLAlchemyError
 from tornado.web import HTTPError
 
@@ -24,7 +28,7 @@ class DatasetHandler(BaseHandler):
     def post(self):
         print("POST to DatasetHandler")
 
-        functions = {'create': getattr(self, 'create_dataset'),
+        functions = {'create': getattr(self, 'create_complete_dataset'),
                      'edit': getattr(self, 'edit_dataset')}
 
         path_elements = [x for x in self.request.path.split("/") if x]
@@ -35,7 +39,7 @@ class DatasetHandler(BaseHandler):
         else:
             raise HTTPError(404)
 
-    def create_dataset(self):
+    def create_complete_dataset(self):
 
         # Get arguments from request
         try:
@@ -51,45 +55,57 @@ class DatasetHandler(BaseHandler):
             testcases_body = testcases_info['body']
 
         except Exception as e:
+            traceback.print_exc()
             raise HTTPError(400) # Bad request
 
+        try:
+            new_dataset = self.create_empty_dataset(memory_limit,
+                                                    name,
+                                                    problem_id,
+                                                    time_limit
+                                                    )
+            # Extract testcases_body
+            extracted = self.extract_zip(testcases_body)
+
+            # Go through files and construct testcases
+            self.create_testcases(extracted, new_dataset)
+            self.session.commit()
+        except Exception as e:
+            traceback.print_exc()
+            self.session.rollback()
+            raise HTTPError(400)
+
+        self.redirect('/problem/' + new_dataset.problem.name)
+
+    def create_testcases(self, extracted, new_dataset):
+        for filename in extracted.keys():
+            print(filename)
+            if filename.endswith('.in'):
+                base = os.path.splitext(filename)[0]
+                in_file = extracted[base + '.in']
+                ok_file = extracted[base + '.ok']
+
+                new_testcase = Testcase(dataset_id=new_dataset.id,
+                                        input_file=in_file,
+                                        output_file=ok_file)
+                self.session.add(new_testcase)
+
+    def create_empty_dataset(self, memory_limit, name, problem_id, time_limit):
         # Create a new dataset with no testcases
-        new_dataset = None
-        try:
-            new_dataset = Dataset(name=name,
-                                  problem_id=problem_id,
-                                  time_limit=time_limit,
-                                  memory_limit=memory_limit)
-            self.session.add(new_dataset)
-            self.session.commit()
-        except SQLAlchemyError as e:
-            print(e)
-            self.redirect('/error/from/datasethandler')
-            return
-
-        # TODO: test if zip, extract zip, get legit testcases etc
-
-        # Create the new testcases
-        try:
-            new_testcase = Testcase(dataset_id=new_dataset.id,
-                                    input_file=testcases_body,
-                                    input_file_digest=b'as',
-                                    output_file=b'output',
-                                    output_file_digest=b'werf')
-            self.session.add(new_testcase)
-            self.session.commit()
-        except SQLAlchemyError as e:
-            print(e)
-            self.redirect('/error/datasethandler')
-            return
-
-        # TODO: test if testcases are added successfully,
-        # TODO: else, delete the dataset
-
-        print(new_dataset)
-        print(new_testcase)
-
-        # self.redirect('/problem/' + new_dataset.problem.name)
+        new_dataset = Dataset(name=name,
+                              problem_id=problem_id,
+                              time_limit=time_limit,
+                              memory_limit=memory_limit)
+        self.session.add(new_dataset)
+        self.session.commit()
+        return new_dataset
 
     def edit_dataset(self):
         pass
+
+    def extract_zip(self, input_zip):
+
+        zip_ref = zipfile.ZipFile(BytesIO(input_zip), 'r')
+
+        return {name: zip_ref.read(name) for name in zip_ref.namelist()}
+
--- DB/Entities/Testcase.py
+++ DB/Entities/Testcase.py
@@ -1,12 +1,25 @@
 # Copyright © 2017 Valentin Rosca <rosca.valentin2012@gmail.com>
 # Copyright © 2017 Cosmin Pascaru <cosmin.pascaru2@gmail.com>
 # Copyright © 2017 Andrei Netedu <andrei.netedu2009@gmail.com>
+import hashlib
 
+import binascii
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy import LargeBinary
 
 from DB.Entities import Base
 
+def get_digest(obj):
+    digest = hashlib.md5(obj).hexdigest()
+    return digest.encode()
+
+def get_input_digest(context):
+    obj = context.current_parameters['input_file']
+    return get_digest(obj)
+
+def get_output_digest(context):
+    obj = context.current_parameters['output_file']
+    return get_digest(obj)
 
 class Testcase(Base):
     __tablename__ = 'testcases'
@@ -21,6 +34,15 @@ class Testcase(Base):
     codename = Column(String)
 
     input_file = Column(LargeBinary, nullable=False)
-    input_file_digest = Column(String, nullable=False)
+    input_file_digest = Column(LargeBinary,
+                               nullable=False,
+                               default=get_input_digest,
+                               onupdate=get_input_digest
+                               )
     output_file = Column(LargeBinary, nullable=False)
-    output_file_digest = Column(String, nullable=False)
+    output_file_digest = Column(LargeBinary,
+                                nullable=False,
+                                default=get_output_digest,
+                                onupdate=get_output_digest
+                                )
+
